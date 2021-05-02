0ddf98c3938e2b29128099abcfb647e1e16aaf77
Dean Rellin <dean.rellin26@gmail.com>
2021-05-02 22:36:41 +0800

    test: redeployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import {
+  Route,
+  Switch,
+  BrowserRouter,
+} from 'react-router-dom';
+import HomePage from './pages';
+import AboutPage from './pages/about';
+import ContactPage from './pages/contact';
+import LoginPage from './pages/login';
+import PrivacyPolicyPage from './pages/privacy-policy';
 
 function App() {
   return (
-    <div className='text-center'>Hello World!</div>
-    // <BrowserRouter>
-    //   <Switch>
-    //     <Route exact path='/' component={HomePage} />
-    //     <Route path='/about' component={AboutPage} />
-    //     <Route path='/contact' component={ContactPage} />
-    //     <Route path='/privacy-policy' component={PrivacyPolicyPage} />
-    //     <Route path='/login' component={LoginPage} />
-    //   </Switch>
-    // </BrowserRouter>
+    <div>  
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Route path='/privacy-policy' component={PrivacyPolicyPage} />
+          <Route path='/login' component={LoginPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
